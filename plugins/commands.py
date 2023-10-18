24541f8ee61ab77fa544307307f4381627d4d4c2
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-10-18 15:45:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,10 +24,10 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url=(MAIN_CHANNEL))
+                InlineKeyboardButton('🖥 𝗡𝗘𝗪 𝗢𝗧𝗧 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 🖥', url=(https://t.me/OTT_ARAKAL_THERAVAD_MOVIES))
             ],
             [
-                InlineKeyboardButton('ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('⭕️𝗚𝗘𝗧 𝗢𝗨𝗥 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞𝗦 ⭕️', url=f"https://t.me/ARAKAL_THERAVAD_GROUP_LINKS"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
